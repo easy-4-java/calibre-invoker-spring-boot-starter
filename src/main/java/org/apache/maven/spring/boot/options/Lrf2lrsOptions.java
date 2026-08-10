b1048391c8e4e4f5d87d6a623505c066814a8f12
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:21:30 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/maven/spring/boot/options/Lrf2lrsOptions.java
+++ src/main/java/org/apache/maven/spring/boot/options/Lrf2lrsOptions.java
@@ -18,7 +18,7 @@ package org.apache.maven.spring.boot.options;
 /**
  * 
  * https://manual.calibre-ebook.com/generated/en/lrf2lrs.html
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class Lrf2lrsOptions {
 
