2562fbc40e1d69b832d2faf4eed7654dba3c4056
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:21:25 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/maven/spring/boot/options/EbookPolishOptions.java
+++ src/main/java/org/apache/maven/spring/boot/options/EbookPolishOptions.java
@@ -23,7 +23,7 @@ package org.apache.maven.spring.boot.options;
  * <p>You should use this tool as the last step in your e-book creation process.</p>
  * <p>Note that polishing only works on files in the AZW3 or EPUB formats.</p>
  * https://manual.calibre-ebook.com/generated/en/ebook-polish.html
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class EbookPolishOptions {
 
