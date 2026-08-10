5397983582a328bdb82c8ab35d15073bc6b7705d
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:21:27 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/maven/spring/boot/options/EbookConvertOptions.java
+++ src/main/java/org/apache/maven/spring/boot/options/EbookConvertOptions.java
@@ -46,7 +46,7 @@ package org.apache.maven.spring.boot.options;
  * then use the -h option.
  * </p>
  * https://manual.calibre-ebook.com/generated/en/ebook-convert.html
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class EbookConvertOptions {
 
--- src/main/java/org/apache/maven/spring/boot/options/EbookMetaOptions.java
+++ src/main/java/org/apache/maven/spring/boot/options/EbookMetaOptions.java
@@ -36,7 +36,7 @@ package org.apache.maven.spring.boot.options;
  * </p>
  * https://manual.calibre-ebook.com/generated/en/ebook-meta.html
  * 
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class EbookMetaOptions {
 
