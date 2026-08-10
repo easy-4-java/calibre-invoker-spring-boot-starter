2a46a74615c13c9def96cc0688ed6bdb4b3340c0
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:21:35 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/maven/spring/boot/CalibreInvokerProperties.java
+++ src/main/java/org/apache/maven/spring/boot/CalibreInvokerProperties.java
@@ -31,7 +31,7 @@ import org.springframework.util.StringUtils;
 /**
  * Calibre Invoker 参数配置
  * 
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @ConfigurationProperties(CalibreInvokerProperties.PREFIX)
 public class CalibreInvokerProperties {
